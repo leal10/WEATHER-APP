cacfb688acb4544045c004dbe8121a940a651742
Daniel Leal <leadaniel456@gmail.com>
2021-06-16 15:46:22 -0500

    Receive more info from the API

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -8,7 +8,9 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      date: ''
+      date: '',
+      temp: '',
+      info: ''
     };
     this.handleStateChange = this.handleStateChange.bind(this);
     this.handleCountryChange = this.handleCountryChange.bind(this);
@@ -35,11 +37,13 @@ class App extends Component {
   handleSubmit(event) {
     event.preventDefault();
     // console.log('api.openweathermap.org/data/2.5/weather?q=Austin&appid=ec28cd50941b65648a3ba3bf1c981500');
-    axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${this.state.city}&appid=ec28cd50941b65648a3ba3bf1c981500`)
+    axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${this.state.city}&units=imperial&appid=${api_key}`)
       .then(results => {
         console.log("this is the results", results.data)
         this.setState({
           info: results.data.weather[0],
+          temp: results.data.main,
+          feels_like: results.data.main.feels_like
         })
       })
       .catch(error => {
@@ -52,9 +56,12 @@ class App extends Component {
     return <div className="Header">
       <Weather />
       <input onChange={this.handleStateChange} placeholder="city" type="text"></input>
-      <input onChange={this.handleCountryChange} placeholder="country"></input>
+      {/* <input onChange={this.handleCountryChange} placeholder="country"></input> */}
       <input onClick={this.handleSubmit} type="submit"></input>
-      <h2>{api_key}</h2>
+      <h2>current temp: {this.state.temp.temp} </h2>
+      <h2>feels like: {this.state.feels_like} </h2>
+      <h2>description: {this.state.info.description} </h2>
+      {/* <h1>&deg;F</h1> */}
     </div>;
   }
 }
